ddcafba243a98af0c96cf2b956c2452490303d14
Ofeoritse Nanna <ofe@Ofeoritses-MBP.attlocal.net>
2021-05-01 11:37:14 -0500

    init commit

---
+++ hearts2.js
@@ -0,0 +1,5 @@
+function addHeart() {
+    const newHeart = document.createElement("div");
+    newHeart.classList.add("hearts");
+    document.body.appendChild(newHeart);
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+function addHeart(){
+    const newHeart = document.createElement("div");
+    newHeart.classList.add("hearts");
+    document.body.appendChild(newHeart);
+ 
+}
+
+
+
+
+
+
